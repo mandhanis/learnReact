2fe77a352315c1f34218641b8feee10dfbc18c96
mandhanis <hilmandhanish@gmail.com>
2023-08-09 16:54:03 +0700

    react

--- hello/src/container/BlogPost/BlogPost.jsx
+++ hello/src/container/BlogPost/BlogPost.jsx
@@ -5,11 +5,18 @@ import axios from 'axios'
 
 class BlogPost extends Component {
     state = {
-        post: []
+        post: [],
+        formBlogPost: {
+            id: 1,
+            title: '',
+            body:'',
+            userId: 1
+        },
+        isUpdate: false
     }
 
     getPostAPI = () => {
-        axios.get('http://localhost:3004/posts')
+        axios.get('http://localhost:3004/posts?_sort=id&_order=desc')
         .then((result) => {
             this.setState({
                 post: result.data
@@ -17,22 +24,71 @@ class BlogPost extends Component {
         })
     }
 
+    postDataToAPI = () => {
+        axios.post('http://localhost:3004/posts', this.state.formBlogPost).then((res) => {
+            console.log(res);
+            this.getPostAPI();
+            this.setState({
+                formBlogPost: {
+                    id: 1,
+                    title: '',
+                    body: '',
+                    userId: 1
+                }
+            })
+        })
+    }
+
+    putDataToAPI = () => {
+        axios.put(`http://localhost:3004/posts/${this.state.formBlogPost}`, this.state.formBlogPost).then((res) => {
+            console.log(res);
+            this.getPostAPI();
+            this.setState({
+                isUpdate: false,
+                formBlogPost: {
+                    id: 1,
+                    title: '',
+                    body: '',
+                    userId: 1
+                }
+            })
+        })
+    }
+
     handleRemove = (data) => {
-        console.log(data);
         axios.delete(`http://localhost:3004/posts/${data}`).then((res) => {
             this.getPostAPI()
         })
     }
 
-    componentDidMount() {
-        // fetch('https://jsonplaceholder.typicode.com/posts')
-        // .then(response => response.json())
-        // .then(json => {
-        //     this.setState({
-        //         post: json
-        //     })
-        // })
+    handleUpdate = (data) => {
+        console.log(data);
+        this.setState({
+            formBlogPost: data,
+            isUpdate: true
+        })
         
+    }
+
+    handleFormChange = (event) => {
+        let formBlogPostNew = {...this.state.formBlogPost};
+        let timestamp = new Date().getTime();
+        formBlogPostNew['id'] = timestamp;
+        formBlogPostNew[event.target.name] = event.target.value;
+        this.setState({
+            formBlogPost: formBlogPostNew
+        })
+    }
+
+    handleSubmit = () => {
+        if (this.isUpdate) {
+            this.putDataToAPI()
+        }else{
+            this.postDataToAPI()
+        }
+    }
+
+    componentDidMount() {
         this.getPostAPI();
     }
 
@@ -40,9 +96,16 @@ class BlogPost extends Component {
         return(
             <Fragment>
                 <p className="section-title">Blog Post</p>
+                <div className="form-add-post">
+                    <label htmlFor="title">Title</label>
+                    <input type="text" value={this.state.formBlogPost.title} name="title" placeholder="add title" onChange={this.handleFormChange}/>
+                    <label htmlFor="body">Blog Content</label>
+                    <textarea name="body" value={this.state.formBlogPost.body} id="body" cols="30" rows="10" placeholder="add blog content" onChange={this.handleFormChange}></textarea>
+                    <button className="btn-submit" onClick={this.handleSubmit}>Simpan</button>
+                </div>
                 {
                     this.state.post.map(post => {
-                        return <Post key={post.id} data={post} remove={this.handleRemove}/>
+                        return <Post key={post.id} data={post} remove={this.handleRemove} update={this.handleUpdate}/>
                          
                     })
                 }
